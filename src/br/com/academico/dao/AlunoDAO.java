289ea6e2d84dd849fe885c012e19333f9d21b05b
prof. Wagner Berto <54516340+profwagnerberto@users.noreply.github.com>
2023-02-06 01:01:02 -0300

    Initial commit

---
+++ src/br/com/academico/dao/AlunoDAO.java
@@ -0,0 +1,51 @@
+package br.com.academico.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+
+import br.com.academico.modelo.Aluno;
+import br.com.aluno.jdbc.FabricaDeConexoes;
+
+public class AlunoDAO {
+  public void criar(Aluno pAluno) throws Exception {
+    Connection con = FabricaDeConexoes.pegarConexao();
+
+    String sql = "insert into tbaluno " + "(cpf, nome, idade, altura) " + "values (?, ?, ?, ?)";
+
+    PreparedStatement stmt = con.prepareStatement(sql);
+
+    stmt.setString(1, pAluno.getCpf());
+    stmt.setString(2, pAluno.getNome());
+    stmt.setInt(3, pAluno.getIdade());
+    stmt.setDouble(4, pAluno.getAltura());
+    stmt.execute();
+  }
+
+  public ArrayList<Aluno> recuperarTodos() throws Exception {
+    Connection con = FabricaDeConexoes.pegarConexao();
+
+    String sql = "select cpf, nome, idade, altura " 
+             + "  from tbaluno";
+
+    PreparedStatement stmt = con.prepareStatement(sql);
+    ResultSet rs = stmt.executeQuery();
+
+    ArrayList<Aluno> alunos = new ArrayList<Aluno>();
+
+    while (rs.next()) {
+      Aluno aluno = new Aluno();
+      aluno.setCpf(rs.getString("cpf"));
+      aluno.setNome(rs.getString("nome"));
+      aluno.setIdade(rs.getInt("idade"));
+      aluno.setAltura(rs.getDouble("altura"));
+
+      alunos.add(aluno);
+    }
+
+    FabricaDeConexoes.encerrarRecursosBD(con, stmt, rs);
+
+    return alunos;
+  }
+}
---
+++ src/br/com/academico/teste/TesteDeCRUD.java
@@ -0,0 +1,41 @@
+package br.com.academico.teste;
+
+import java.util.ArrayList;
+
+import br.com.academico.dao.AlunoDAO;
+import br.com.academico.modelo.Aluno;
+
+public class TesteDeCRUD {
+
+  public static void main(String[] args) throws Exception {
+    try {
+      Aluno aluno1 = new Aluno();
+
+      aluno1.setCpf("1");
+      aluno1.setNome("Henzo");
+      aluno1.setIdade(20);
+      aluno1.setAltura(1.81);
+
+      System.out.println("Aluno criado e preenchido");
+
+      AlunoDAO alunoDAO1 = new AlunoDAO();
+      System.out.println("Inserido por objeto");
+      
+      ArrayList<Aluno> alunos = alunoDAO1.recuperarTodos();
+      
+      System.out.printf("CPF         | NOME            | IDADE | ALTURA\n");
+      System.out.printf("==============================================\n");
+      for (Aluno auxiliar : alunos)
+        System.out.printf("%-11s | %-15s | %5d | %6.2f\n",
+            auxiliar.getCpf(),
+            auxiliar.getNome(),
+            auxiliar.getIdade(),
+            auxiliar.getAltura());
+    } catch (Exception e) {
+      System.err.println("Erro no banco de dados: " + "\n" + 
+          e.getMessage() + "\n" + 
+          e.getClass());
+//    e.printStackTrace();
+    }
+  }
+}
